fdef680bec49321d1e66120ff30d0740e4f3ac30
Dannar Mawardi <dannar@Dannars-MacBook-Pro.local>
2024-08-19 11:26:15 +0800

    Added Course factory

---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user = User::factory()->create();
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'created_by' => $user->id,
+        ];
+    }
+}
